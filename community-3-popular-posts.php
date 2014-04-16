d83544b9bcc8b5ab1b0c9a9a2f9554f4c6e51422
U-lenovo-PC\lenovo <lenovo@lenovo-PC.(none)>
2014-04-16 21:57:28 +0800

    community 3 popular posts as widget

---
+++ community-3-popular-posts.php
@@ -0,0 +1,65 @@
+<?php
+widget_css();
+
+$posts = g::posts(
+	array(
+		'domain'				=> etc::domain(),
+		'wr_datetime'=> '>=' . g::datetime( time() - ONEDAY * 7),
+		'order by'=>'wr_hit DESC',
+		'limit'=>4
+	)
+);
+?>
+<div class='popular-posts'>
+	<div class='title'>
+		<table width='100%'>
+			<tr valign='top'>
+				<td align='left' class='title-left'>
+					<img src="<?=x::url_theme()?>/img/popular-posts.png">
+					<span class='label'>조회수가 많은 글</span>
+				</td>
+			</tr>
+		</table>
+	</div>
+	
+	<div class='popular-posts-items'>
+		<?php
+		if ( $posts ) {			
+			$i = 0;
+			foreach ( $posts as $p ) {
+					$url = G5_BBS_URL."/board.php?bo_table=$p[bo_table]&wr_id=$p[wr_id]";
+					$popular_subject = conv_subject( $p['wr_subject'], 15, '...');
+					$no_of_views = $p['wr_hit'];
+					$no_of_comments = $p['wr_comment'];
+					
+						if ( $i == (count($posts)-1) ) $last_post = 'last-post';
+						else $last_post = null;
+						$i++;
+						echo "
+								<div class='row $last_post'>
+									<span class='post-num'>$i</span>
+									<span><a href='$url'>$popular_subject</a></span>
+								</div>
+								
+						";
+			 }
+		}
+		else {
+			echo "
+					<div class='row'>
+						<span class='post-num'>1</span>
+						<span><a href='javascript:void(0)'>회원님께서는 현재</a></span>
+					</div>
+					<div class='row'>
+						<span class='post-num'>2</span>
+						<span><a href='javascript:void(0)'>필고 커뮤니티 테마를</a></span>
+					</div>
+					<div class='row'>
+						<span class='post-num'>3</span>
+						<span><a href='javascript:void(0)'>사용 중 입니다.</a></span>
+					</div>						
+				";
+		}
+		?>
+	</div>
+</div>
